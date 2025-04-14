5d80c644403ff8f0645ca334d1244ab92e9ef543
Matthew Skinner <skinner@Matthews-MacBook-Pro.local>
2025-04-14 08:54:08 -0500

    updating

--- .export.sh
+++ .export.sh
@@ -1,3 +1,2 @@
-export PATH="$HOME/.local/bin:/usr/local/bin:/usr/bin:/usr/sbin:/sbin:/bin:$HOME/.docker/bin:$PATH"
 export STARSHIP_CONFIG="$HOME/dotfiles/starship.toml"
 export GIT_CONFIG_GLOBAL="~/dotfiles/.gitconfig"
